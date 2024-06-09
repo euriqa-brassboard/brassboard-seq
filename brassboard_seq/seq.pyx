a6daeb0d843f13af96d96b3b7d746d87bd7b782b
Yichao Yu <yyc1992@gmail.com>
2024-06-08 21:54:53 -0500

    Functions to add steps and subsequences

--- brassboard_seq/seq.pyx
+++ brassboard_seq/seq.pyx
@@ -3,10 +3,11 @@
 # Do not use relative import since it messes up cython file name tracking
 from brassboard_seq.config cimport translate_channel
 from brassboard_seq.rtval cimport convert_bool
-from brassboard_seq.utils cimport assume_not_none, event_time_key
+from brassboard_seq.utils cimport assume_not_none, _assume_not_none, \
+  event_time_key
 
 cimport cython
-from cpython cimport PyErr_Format, PyObject, PyDict_GetItemWithError, PyList_GET_SIZE
+from cpython cimport PyErr_Format, PyObject, PyDict_GetItemWithError, PyList_GET_SIZE, PyTuple_GET_SIZE, PyDict_Size
 
 cdef combine_cond(cond1, new_cond):
     if cond1 is False:
@@ -48,6 +49,28 @@ cdef class ConditionalWrapper:
     def wait(self, length, *, cond=True):
         wait_cond(self.seq, length, combine_cond(self.cond, cond))
 
+    def add_step(self, first_arg, *args, **kwargs):
+        seq = self.seq
+        step = add_step_real(seq, self.cond, seq.end_time, first_arg, args, kwargs)
+        seq.end_time = step.end_time
+        return step
+
+    def add_background(self, first_arg, *args, **kwargs):
+        seq = self.seq
+        return add_step_real(seq, self.cond, seq.end_time, first_arg, args, kwargs)
+
+    def add_floating(self, first_arg, *args, **kwargs):
+        seq = self.seq
+        cond = self.cond
+        return add_step_real(seq, cond,
+                             seq.seqinfo.time_mgr.new_time_int(None, 0, True,
+                                                               cond, None),
+                             first_arg, args, kwargs)
+
+    def add_at(self, EventTime tp, first_arg, *args, **kwargs):
+        seq = self.seq
+        return add_step_real(seq, self.cond, tp, first_arg, args, kwargs)
+
 cdef class SubSeq(TimeSeq):
     def __init__(self):
         PyErr_Format(TypeError, "SubSeq cannot be created directly")
@@ -61,11 +84,63 @@ cdef class SubSeq(TimeSeq):
     def wait(self, length, *, cond=True):
         wait_cond(self, length, combine_cond(self.cond, cond))
 
+    def add_step(self, first_arg, *args, **kwargs):
+        step = add_step_real(self, self.cond, self.end_time, first_arg, args, kwargs)
+        self.end_time = step.end_time
+        return step
+
+    def add_background(self, first_arg, *args, **kwargs):
+        return add_step_real(self, self.cond, self.end_time, first_arg, args, kwargs)
+
+    def add_floating(self, first_arg, *args, **kwargs):
+        cond = self.cond
+        return add_step_real(self, cond,
+                             self.seqinfo.time_mgr.new_time_int(None, 0, True,
+                                                                cond, None),
+                             first_arg, args, kwargs)
+
+    def add_at(self, EventTime tp, first_arg, *args, **kwargs):
+        return add_step_real(self, self.cond, tp, first_arg, args, kwargs)
+
 cdef int wait_cond(SubSeq self, length, cond) except -1:
     self.end_time = self.seqinfo.time_mgr.new_round_time(self.end_time, length,
                                                          False, cond, None)
     self.seqinfo.bt_tracker.record(event_time_key(<void*>self.end_time))
     return 0
+
+cdef SubSeq add_custom_step(SubSeq self, cond, EventTime start_time, cb,
+                            tuple args, dict kwargs):
+    subseq = <SubSeq>SubSeq.__new__(SubSeq)
+    init_subseq(subseq, self, start_time, cond)
+    cb(subseq, *args, **kwargs)
+    _assume_not_none(<void*>self.sub_seqs)
+    self.sub_seqs.append(subseq)
+    return subseq
+
+cdef TimeStep add_time_step(SubSeq self, cond, EventTime start_time, length):
+    step = <TimeStep>TimeStep.__new__(TimeStep)
+    init_timeseq(step, self, start_time, cond)
+    step.actions = {}
+    step.length = length
+    step.end_time = self.seqinfo.time_mgr.new_round_time(start_time, length,
+                                                         False, cond, None)
+    self.seqinfo.bt_tracker.record(event_time_key(<void*>step.end_time))
+    _assume_not_none(<void*>self.sub_seqs)
+    self.sub_seqs.append(step)
+    return step
+
+cdef TimeSeq add_step_real(SubSeq self, cond, EventTime start_time,
+                           first_arg, tuple args, dict kwargs):
+    if callable(first_arg):
+        return add_custom_step(self, cond, start_time, first_arg, args, kwargs)
+    elif not PyTuple_GET_SIZE(args) and not PyDict_Size(kwargs):
+        return add_time_step(self, cond, start_time, first_arg)
+    else:
+        sargs = str(args)
+        skwargs = str(kwargs)
+        PyErr_Format(ValueError,
+                     "Unexpected arguments when creating new time step, %U, %U.",
+                     <PyObject*>sargs, <PyObject*>skwargs)
 
 @cython.no_gc
 @cython.final
--- tests/test_seq.py
+++ tests/test_seq.py
@@ -84,3 +84,268 @@ def test_seq():
     t5 = s.end_time
     assert test_utils.event_time_id(t5) == 5
     assert str(t5) == 'T[4] + (1 s; if False)'
+
+    st1 = s.add_step(0.2)
+    assert st1.start_time is t5
+    assert st1.end_time is s.end_time
+    t6 = s.end_time
+    assert test_utils.event_time_id(t6) == 6
+    assert str(t6) == 'T[5] + 200 ms'
+
+    st2 = s.add_background(10)
+    assert st2.start_time is t6
+    assert s.end_time is t6
+    t7 = st2.end_time
+    assert test_utils.event_time_id(t7) == 7
+    assert str(t7) == 'T[6] + 10 s'
+
+    st3 = s.add_floating(0.1)
+    t8 = st3.start_time
+    t9 = st3.end_time
+    assert s.end_time is t6
+    assert test_utils.event_time_id(t8) == 8
+    assert test_utils.event_time_id(t9) == 9
+    assert str(t8) == '<floating>'
+    assert str(t9) == 'T[8] + 100 ms'
+
+    st4 = s.add_at(t9, 0.12)
+    assert st4.start_time is t9
+    t10 = st4.end_time
+    assert s.end_time is t6
+    assert test_utils.event_time_id(t10) == 10
+    assert str(t10) == 'T[9] + 120 ms'
+
+    st5 = c2.add_step(0.2)
+    assert st5.start_time is t6
+    assert st5.end_time is s.end_time
+    t11 = s.end_time
+    assert test_utils.event_time_id(t11) == 11
+    assert str(t11) == f'T[6] + (200 ms; if {bv1})'
+
+    st6 = c2.add_background(10)
+    assert st6.start_time is t11
+    assert s.end_time is t11
+    t12 = st6.end_time
+    assert test_utils.event_time_id(t12) == 12
+    assert str(t12) == f'T[11] + (10 s; if {bv1})'
+
+    st7 = c2.add_floating(0.1)
+    t13 = st7.start_time
+    t14 = st7.end_time
+    assert s.end_time is t11
+    assert test_utils.event_time_id(t13) == 13
+    assert test_utils.event_time_id(t14) == 14
+    assert str(t13) == '<floating>'
+    assert str(t14) == f'T[13] + (100 ms; if {bv1})'
+
+    st8 = c2.add_at(t14, 0.12)
+    assert st8.start_time is t14
+    t15 = st8.end_time
+    assert s.end_time is t11
+    assert test_utils.event_time_id(t15) == 15
+    assert str(t15) == f'T[14] + (120 ms; if {bv1})'
+
+    st9 = s.add_step(step1, 0.1, cond1=True)
+    assert st9.start_time is t11
+    assert st9.end_time is s.end_time
+    t16 = s.end_time
+    assert test_utils.event_time_id(t16) == 16
+    assert str(t16) == 'T[11] + 100 ms'
+
+    st10 = s.add_background(step2, 0.2, cond2=bv1)
+    assert st10.start_time is t16
+    assert s.end_time is t16
+    t19 = st10.end_time
+    assert test_utils.event_time_id(t19) == 19
+    assert str(t19) == f'T[18] + (200 ms; if {bv1})'
+
+    st11 = s.add_floating(step1, v1, cond1=False)
+    t20 = st11.start_time
+    t21 = st11.end_time
+    assert s.end_time is t16
+    assert test_utils.event_time_id(t20) == 20
+    assert test_utils.event_time_id(t21) == 21
+    assert str(t20) == '<floating>'
+    assert str(t21) == f'T[20] + int64({v1} * 1000000000000)'
+
+    st12 = s.add_at(t21, step2, v2, cond2=False)
+    assert st12.start_time is t21
+    t24 = st12.end_time
+    assert s.end_time is t16
+    assert test_utils.event_time_id(t24) == 24
+    assert str(t24) == f'T[23] + (int64({v2} * 1000000000000); if False)'
+
+    st13 = c2.add_step(step1, 0.1, cond1=True)
+    assert st13.start_time is t16
+    assert st13.end_time is s.end_time
+    t25 = s.end_time
+    assert test_utils.event_time_id(t25) == 25
+    assert str(t25) == f'T[16] + (100 ms; if {bv1})'
+
+    st14 = c2.add_background(step2, 0.2, cond2=v2)
+    assert st14.start_time is t25
+    assert s.end_time is t25
+    t28 = st14.end_time
+    assert test_utils.event_time_id(t28) == 28
+    assert str(t28) == f'T[27] + (200 ms; if {bv1} & bool({v2}))'
+
+    st15 = c2.add_floating(step1, v1, cond1=False)
+    t29 = st15.start_time
+    t30 = st15.end_time
+    assert s.end_time is t25
+    assert test_utils.event_time_id(t29) == 29
+    assert test_utils.event_time_id(t30) == 30
+    assert str(t29) == '<floating>'
+    assert str(t30) == f'T[29] + (int64({v1} * 1000000000000); if {bv1})'
+
+    st16 = c2.add_at(t30, step2, v2, cond2=False)
+    assert st16.start_time is t30
+    t33 = st16.end_time
+    assert s.end_time is t25
+    assert test_utils.event_time_id(t33) == 33
+    assert str(t33) == f'T[32] + (int64({v2} * 1000000000000); if False)'
+
+def test_seq_error1():
+    conf = Config()
+    s = seq.Seq(conf)
+    c1 = s.conditional(True)
+
+    with pytest.raises(TypeError):
+        s.add_step(step1)
+    with pytest.raises(TypeError):
+        s.add_step(step1, 1, 2)
+    with pytest.raises(TypeError):
+        s.add_step(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        s.add_background(step1)
+    with pytest.raises(TypeError):
+        s.add_background(step1, 1, 2)
+    with pytest.raises(TypeError):
+        s.add_background(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        s.add_floating(step1)
+    with pytest.raises(TypeError):
+        s.add_floating(step1, 1, 2)
+    with pytest.raises(TypeError):
+        s.add_floating(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        s.add_at(s.start_time, step1)
+    with pytest.raises(TypeError):
+        s.add_at(s.start_time, step1, 1, 2)
+    with pytest.raises(TypeError):
+        s.add_at(s.start_time, step1, 1, cond2=True)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_step(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_step(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_background(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_background(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_floating(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_floating(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_at(s.start_time, 1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        s.add_at(s.start_time, 1, a=2)
+
+    with pytest.raises(TypeError):
+        c1.add_step(step1)
+    with pytest.raises(TypeError):
+        c1.add_step(step1, 1, 2)
+    with pytest.raises(TypeError):
+        c1.add_step(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        c1.add_background(step1)
+    with pytest.raises(TypeError):
+        c1.add_background(step1, 1, 2)
+    with pytest.raises(TypeError):
+        c1.add_background(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        c1.add_floating(step1)
+    with pytest.raises(TypeError):
+        c1.add_floating(step1, 1, 2)
+    with pytest.raises(TypeError):
+        c1.add_floating(step1, 1, cond2=True)
+
+    with pytest.raises(TypeError):
+        c1.add_at(s.start_time, step1)
+    with pytest.raises(TypeError):
+        c1.add_at(s.start_time, step1, 1, 2)
+    with pytest.raises(TypeError):
+        c1.add_at(s.start_time, step1, 1, cond2=True)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_step(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_step(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_background(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_background(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_floating(1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_floating(1, a=2)
+
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_at(s.start_time, 1, 2)
+    with pytest.raises(ValueError, match="Unexpected arguments"):
+        c1.add_at(s.start_time, 1, a=2)
+
+def step1(s, len1, *, cond1=None):
+    t1 = s.start_time
+    assert s.end_time is t1
+    tid1 = test_utils.event_time_id(t1)
+
+    step = s.add_step(len1)
+
+    assert step.start_time is t1
+    t2 = step.end_time
+    tid2 = test_utils.event_time_id(t2)
+    assert tid2 >= tid1 + 1
+    assert s.end_time is t2
+
+    c1 = s.conditional(cond1)
+    step2 = c1.add_at(t1, len1 * 2)
+
+    assert step2.start_time is t1
+    assert s.end_time is t2
+    t3 = step2.end_time
+    tid3 = test_utils.event_time_id(t3)
+    assert tid3 == tid2 + 1
+
+def step2(s, len2, *, cond2=None):
+    t1 = s.start_time
+    assert s.end_time is t1
+    tid1 = test_utils.event_time_id(t1)
+
+    step = s.add_step(len2 * 2)
+
+    assert step.start_time is t1
+    t2 = step.end_time
+    tid2 = test_utils.event_time_id(t2)
+    assert tid2 >= tid1 + 1
+    assert s.end_time is t2
+
+    c1 = s.conditional(cond2)
+    step2 = c1.add_step(len2)
+
+    assert step2.start_time is t2
+    t3 = step2.end_time
+    tid3 = test_utils.event_time_id(t3)
+    assert tid3 == tid2 + 1
+    assert s.end_time is t3
