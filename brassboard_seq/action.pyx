d1ec4e8d1394e73ee381848ea00b5d7e26404ade
Yichao Yu <yyc1992@gmail.com>
2024-06-11 11:27:54 -0400

    Add ramp function base class
    Also add buffer for evaluation from C code.

--- brassboard_seq/action.pyx
+++ brassboard_seq/action.pyx
@@ -1,7 +1,12 @@
 # cython: language_level=3
 
+# Do not use relative import since it messes up cython file name tracking
+from brassboard_seq.rtval cimport get_value
+
 cimport cython
 from cpython cimport PyErr_Format, Py_LT, Py_GT
+
+cimport numpy as cnpy
 
 @cython.no_gc
 @cython.final
@@ -36,3 +41,74 @@ cdef class Action:
 
     def __repr__(self):
         return str(self)
+
+cdef class RampFunction:
+    def __init__(self, *, **params):
+        self.params = params
+
+    @cython.final
+    cdef int set_compile_params(self) except -1:
+        for (name, value) in self.params.items():
+            setattr(self, name, value)
+
+    @cython.final
+    cdef int set_runtime_params(self, long long age) except -1:
+        for (name, value) in self.params.items():
+            setattr(self, name, get_value(value, age))
+
+    cpdef eval(self, t, length, oldval):
+        return 0.0
+
+@cython.final
+cdef class RampBuffer:
+    def __init__(self):
+        PyErr_Format(TypeError, "RampBuffer cannot be created directly")
+
+    cdef double *alloc_input(self, int size) except NULL:
+        cdef cnpy.npy_intp dims[1]
+        dims[0] = size
+        cdef cnpy.PyArray_Dims pydims
+        pydims.ptr = dims
+        pydims.len = 1
+        cdef cnpy.ndarray buff
+        if self.input_buff is None:
+            buff = <cnpy.ndarray>cnpy.PyArray_EMPTY(1, dims, cnpy.NPY_DOUBLE, 0)
+            self.input_buff = buff
+        else:
+            buff = <cnpy.ndarray>self.input_buff
+            cnpy.PyArray_Resize(buff, &pydims, 0, cnpy.NPY_CORDER)
+        return <double*>cnpy.PyArray_DATA(buff)
+
+    cdef double *eval(self, RampFunction func, double length,
+                      double oldval) except NULL:
+        buff = <cnpy.ndarray?>func.eval(self.input_buff, length, oldval)
+        if buff.ndim != 1 or buff.size != len(self.input_buff):
+            PyErr_Format(ValueError, "Ramp result dimension mismatch")
+        if cnpy.PyArray_TYPE(buff) != cnpy.NPY_DOUBLE:
+            buff = <cnpy.ndarray>cnpy.PyArray_Cast(buff, cnpy.NPY_DOUBLE)
+        self.output_buff = buff
+        return <double*>cnpy.PyArray_DATA(buff)
+
+cdef RampBuffer new_ramp_buffer():
+    cnpy.import_array()
+    buff = <RampBuffer>RampBuffer.__new__(RampBuffer)
+    return buff
+
+cdef double *rampbuffer_alloc_input(PyObject *_self, int size) noexcept:
+    cdef RampBuffer self = <RampBuffer>_self
+    self.exception = None
+    try:
+        return self.alloc_input(size)
+    except BaseException as e:
+        self.exception = e
+        return NULL
+
+cdef double *rampbuffer_eval(PyObject *_self, PyObject *_func,
+                             double length, double oldval) noexcept:
+    cdef RampBuffer self = <RampBuffer>_self
+    self.exception = None
+    try:
+        return self.eval(<RampFunction?>_func, length, oldval)
+    except BaseException as e:
+        self.exception = e
+        return NULL
--- tests/brassboard_seq_test_utils.pyx
+++ tests/brassboard_seq_test_utils.pyx
@@ -1,9 +1,41 @@
 # cython: language_level=3
 
 from brassboard_seq cimport action
+from cpython cimport PyObject
+
+import numpy as np
 
 def new_action(value, cond, bint is_pulse, bint exact_time, dict kws):
     return action.new_action(value, cond, is_pulse, exact_time, kws)
 
 def action_set_tid(action.Action action, int tid):
     action.tid = tid
+
+cdef class RampBufferTest:
+    cdef action.RampFunction func
+    cdef action.RampBuffer buff
+    def __init__(self, func):
+        self.func = func
+        self.buff = action.new_ramp_buffer()
+
+    def eval_compile(self, t, length, oldval):
+        self.func.set_compile_params()
+        return self.func.eval(t, length, oldval)
+
+    def eval_runtime(self, age, ts, length, oldval):
+        self.func.set_runtime_params(age)
+        nt = len(ts)
+        cdef double *buff_ptr = action.rampbuffer_alloc_input(<PyObject*>self.buff, nt)
+        if buff_ptr == NULL:
+            raise self.buff.exception
+        for i in range(nt):
+            buff_ptr[i] = ts[i]
+        cdef double *output_ptr = action.rampbuffer_eval(<PyObject*>self.buff,
+                                                         <PyObject*>self.func,
+                                                         length, oldval)
+        if output_ptr == NULL:
+            raise self.buff.exception
+        output = np.empty(nt)
+        for i in range(nt):
+            output[i] = output_ptr[i]
+        return output
