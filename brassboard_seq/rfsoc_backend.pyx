55efd0aaa07114515908df354ef533a3301ceb57
Yichao Yu <yyc1992@gmail.com>
2024-08-04 11:06:08 -0400

    Add license header

--- brassboard_seq/rfsoc_backend.pyx
+++ brassboard_seq/rfsoc_backend.pyx
@@ -1,5 +1,21 @@
 # cython: language_level=3
 
+# Copyright (c) 2024 - 2024 Yichao Yu <yyc1992@gmail.com>
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not,
+# see <http://www.gnu.org/licenses/>.
+
 # Do not use relative import since it messes up cython file name tracking
 from brassboard_seq.action cimport Action, RampFunction, SeqCubicSpline, \
   new_ramp_buffer, ramp_get_spline_segments, rampbuffer_alloc_input, rampbuffer_eval
--- brassboard_seq/seq.pxd
+++ brassboard_seq/seq.pxd
@@ -1,5 +1,21 @@
 # cython: language_level=3
 
+# Copyright (c) 2024 - 2024 Yichao Yu <yyc1992@gmail.com>
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not,
+# see <http://www.gnu.org/licenses/>.
+
 # Do not use relative import since it messes up cython file name tracking
 from brassboard_seq.config cimport Config
 from brassboard_seq.event_time cimport TimeManager, EventTime, new_time_manager
