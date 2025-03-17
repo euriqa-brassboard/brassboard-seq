c1845f066c5ba68169878b619b1b0843f96a0921
Yichao Yu <yyc1992@gmail.com>
2025-03-17 10:54:20 -0400

    Move more rfsoc code out of the header

--- brassboard_seq/src/rfsoc_backend.h
+++ brassboard_seq/src/rfsoc_backend.h
@@ -47,12 +47,6 @@ struct cubic_spline_t {
     }
 };
 
-struct output_flags_t {
-    bool wait_trigger;
-    bool sync;
-    bool feedback_enable;
-};
-
 enum ToneParam {
     ToneFreq,
     TonePhase,
@@ -205,96 +199,6 @@ struct Generator {
     virtual void end() = 0;
     virtual ~Generator() = default;
 };
-
-static inline int64_t seq_time_to_cycle(long long time)
-{
-    // Each cycle is 1 / 1ps / 409.6MHz sequence time unit
-    // or 78125/32 sequence unit.
-    constexpr auto numerator = 78125;
-    constexpr auto denominator = 32;
-    auto cycle_whole = time / numerator * denominator;
-    auto cycle_frac = ((time % numerator) * denominator + numerator / 2) / numerator;
-    return cycle_whole + cycle_frac;
-}
-
-static inline int64_t cycle_to_seq_time(int64_t cycle)
-{
-    constexpr auto numerator = 32;
-    constexpr auto denominator = 78125;
-    auto time_whole = cycle / numerator * denominator;
-    auto time_frac = ((cycle % numerator) * denominator + numerator / 2) / numerator;
-    return time_whole + time_frac;
-}
-
-static __attribute__((always_inline)) inline
-cubic_spline_t spline_from_static(double v0)
-{
-    return { v0, 0, 0, 0 };
-}
-
-static __attribute__((optimize("-ffast-math"),always_inline)) inline
-cubic_spline_t spline_from_values(double v0, double v1, double v2, double v3)
-{
-    // v = o0 + o1 * t + o2 * t^2 + o3 * t^3
-
-    // v0 = o0
-    // v1 = o0 + o1 / 3 + o2 / 9 + o3 / 27
-    // v2 = o0 + o1 * 2 / 3 + o2 * 4 / 9 + o3 * 8 / 27
-    // v3 = o0 + o1 + o2 + o3
-
-    // o0 = v0
-    // o1 = -5.5 * v0 + 9 * v1 - 4.5 * v2 + v3
-    // o2 = 9 * v0 - 22.5 * v1 + 18 * v2 - 4.5 * v3
-    // o3 = -4.5 * v0 + 13.5 * v1 - 13.5 * v2 + 4.5 * v3
-
-    return {
-        v0,
-        -5.5 * v0 + 9 * v1 - 4.5 * v2 + v3,
-        9 * v0 - 22.5 * v1 + 18 * v2 - 4.5 * v3,
-        -4.5 * v0 + 13.5 * v1 - 13.5 * v2 + 4.5 * v3,
-    };
-}
-
-static __attribute__((optimize("-ffast-math"),always_inline)) inline
-double spline_eval(cubic_spline_t spline, double t)
-{
-    return spline.order0 + (spline.order1 +
-                            (spline.order2 + spline.order3 * t) * t) * t;
-}
-
-static __attribute__((optimize("-ffast-math"),always_inline)) inline
-cubic_spline_t spline_resample(cubic_spline_t spline, double t1, double t2)
-{
-    double dt = t2 - t1;
-    double dt2 = dt * dt;
-    double dt3 = dt2 * dt;
-    double o3_3 = 3 * spline.order3;
-    return {
-        spline.order0 + (spline.order1 +
-                         (spline.order2 + spline.order3 * t1) * t1) * t1,
-        dt * (spline.order1 + (2 * spline.order2 + o3_3 * t1) * t1),
-        dt2 * (spline.order2 + o3_3 * t1),
-        dt3 * spline.order3,
-    };
-}
-
-static inline cubic_spline_t approximate_spline(double v[5])
-{
-    double v0 = v[0];
-    double v1 = v[1] * (2.0 / 3) + v[2] * (1.0 / 3);
-    double v2 = v[3] * (2.0 / 3) + v[2] * (1.0 / 3);
-    double v3 = v[4];
-    // clamp v1 and v2 so that the numbers won't go too crazy
-    if (v3 >= v0) {
-        v1 = std::clamp(v1, v0, v3);
-        v2 = std::clamp(v2, v0, v3);
-    }
-    else {
-        v1 = std::clamp(v1, v3, v0);
-        v2 = std::clamp(v2, v3, v0);
-    }
-    return spline_from_values(v0, v1, v2, v3);
-}
 
 }
 
