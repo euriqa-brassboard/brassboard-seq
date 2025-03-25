1a5ef7d5ed6fdbe1bdf81fb0b5fc5ae1254b35bd
Yichao Yu <yyc1992@gmail.com>
2025-03-25 09:37:21 -0400

    Update parampack benchmark

--- benchmarks/bench_parampack.py
+++ benchmarks/bench_parampack.py
@@ -3,13 +3,13 @@
 from brassboard_seq.scan import ParamPack
 
 def test():
-    ParamPack()
+    # ParamPack()
     # ParamPack(a=2, b=3)
-    # p = ParamPack(a=2, b=3)
-    # p.a(3)
-    # p2 = p.c(c=2)
-    # p2.c(10)
-    # p.b()
+    p = ParamPack(a=2, b=3)
+    p.a(3)
+    p2 = p.c(c=2)
+    p2.c(10)
+    p.b()
 
 for i in range(10_000_000):
     test()
