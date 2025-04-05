6d335f3cb15f4e81a7c79afe5c5da7df96b30357
Yichao Yu <yyc1992@gmail.com>
2025-04-05 15:33:05 -0400

    Move event_time implementation into C++

--- brassboard_seq/event_time.pyx
+++ brassboard_seq/event_time.pyx
@@ -17,18 +17,10 @@
 # see <http://www.gnu.org/licenses/>.
 
 # Do not use relative import since it messes up cython file name tracking
-from brassboard_seq.rtval cimport new_const, new_extern_age, rt_eval_throw, \
-  ExternCallback
-from brassboard_seq.utils cimport _assume_not_none, assume_not_none, \
-  event_time_key, bb_err_format, PyExc_RuntimeError, PyExc_TypeError
-
-from libcpp.map cimport map as cppmap
-from libcpp.algorithm cimport fill as cppfill
-from libcpp.unordered_set cimport unordered_set
+from brassboard_seq.rtval cimport new_const, new_extern_age, ExternCallback
+from brassboard_seq.utils cimport PyExc_RuntimeError, PyExc_TypeError
 
 cimport cython
-from cython.operator cimport dereference as deref, predecrement as predec
-from cpython cimport PyList_GET_SIZE
 
 cdef object py_time_scale = c_time_scale
 cdef RuntimeValue rt_time_scale = new_const(py_time_scale, <RuntimeValue>None)
@@ -37,59 +29,14 @@ def time_scale():
     return py_time_scale
 
 cdef extern from "src/event_time.cpp" namespace "brassboard_seq::event_time":
-    bint get_cond_val(PyObject*, unsigned age, py_object &pyage) except +
     str str_time(long long) except +
+    void timemanager_finalize(TimeManager self, EventTime) except +
+    long long timemanager_compute_all_times(TimeManager self, unsigned age,
+                                            py_object &pyage, EventTime) except +
+    double timediff_eval(EventTimeDiff self, unsigned age) except +
     void update_event_time_gc_callback(object type, EventTime)
 
 update_event_time_gc_callback(EventTime, None)
-
-cdef int visit_time(TimeManager self, EventTime t, unordered_set[int] &visited) except -1:
-    cdef int id = t.data.id
-    if id < 0:
-        return 0
-    if t.data.floating:
-        bb_err_format(RuntimeError, event_time_key(<void*>t),
-                      "Event time still floating")
-    if visited.count(id):
-        bb_err_format(ValueError, event_time_key(<void*>t), "Time loop detected")
-    visited.insert(id)
-    cdef long long static_offset
-    p_rt_offset = t.data.get_rt_offset()
-    cond = t.cond
-    if cond is True:
-        if p_rt_offset != NULL:
-            static_offset = -1
-        else:
-            static_offset = <long long>t.data.get_c_offset()
-    elif cond is False:
-        static_offset = 0
-    elif p_rt_offset == NULL and t.data.get_c_offset() == 0:
-        static_offset = 0
-    else:
-        static_offset = -1
-
-    cdef long long static_prev = 0
-    cdef long long static_wait_for = 0
-    prev = t.prev
-    if prev is not None:
-        visit_time(self, prev, visited)
-        static_prev = prev.data.get_static()
-    wait_for = t.wait_for
-    if wait_for is not None:
-        visit_time(self, wait_for, visited)
-        static_wait_for = wait_for.data.get_static()
-        if cond is False:
-            if static_prev != -1:
-                t.data.set_static(static_prev)
-        elif (cond is True and static_wait_for != -1 and
-              static_offset != -1 and static_prev != -1):
-            t.data.set_static(max(static_prev, static_wait_for + static_offset))
-    elif static_offset != -1 and static_prev != -1:
-        t.data.set_static(static_prev + static_offset)
-    t.data.id = -1
-    _assume_not_none(<void*>self.event_times)
-    self.event_times.append(t)
-    return 0
 
 @cython.auto_pickle(False)
 @cython.final
@@ -98,180 +45,10 @@ cdef class TimeManager:
         PyErr_Format(PyExc_TypeError, "TimeManager cannot be created directly")
 
     cdef int finalize(self) except -1:
-        status = self.status.get()
-        if status.finalized:
-            PyErr_Format(PyExc_RuntimeError, "Event times already finalized")
-        status.finalized = True
-        old_event_times = self.event_times
-        event_times = []
-        self.event_times = event_times
-        cdef unordered_set[int] visited
-        # First, topologically order the times
-        assume_not_none(old_event_times)
-        for _t in old_event_times:
-            visit_time(self, <EventTime>_t, visited)
-
-        cdef vector[int] chain_lengths
-        cdef int i = 0
-        cdef int chain_id1
-        cdef int chain_pos1
-        cdef int chain_id2
-        cdef int chain_pos2
-        cdef int chain_id
-        cdef int chain_pos
-        cdef EventTime t
-        assume_not_none(event_times)
-        for _t in event_times:
-            t = <EventTime>_t
-            tid = i
-            i += 1
-            t.data.id = tid
-            prev = t.prev
-            if prev is not None:
-                chain_id1 = prev.data.chain_id
-                chain_pos1 = prev.chain_pos[chain_id1]
-                if chain_pos1 + 1 != chain_lengths[chain_id1]:
-                    # Not the tail of the chain
-                    chain_id1 = -1
-            else:
-                chain_id1 = -1
-            wait_for = t.wait_for
-            if wait_for is not None:
-                chain_id2 = wait_for.data.chain_id
-                chain_pos2 = wait_for.chain_pos[chain_id2]
-                if chain_pos2 + 1 != chain_lengths[chain_id2]:
-                    # Not the tail of the chain
-                    chain_id2 = -1
-            else:
-                chain_id2 = -1
-            if chain_id1 == -1:
-                if chain_id2 == -1:
-                    # New chain
-                    chain_id = chain_lengths.size()
-                    chain_pos = 0
-                    chain_lengths.push_back(0)
-                else:
-                    chain_id = chain_id2
-                    chain_pos = chain_pos2 + 1
-            elif chain_id2 == -1:
-                chain_id = chain_id1
-                chain_pos = chain_pos1 + 1
-            elif chain_pos2 > chain_pos1:
-                chain_id = chain_id2
-                chain_pos = chain_pos2 + 1
-            else:
-                chain_id = chain_id1
-                chain_pos = chain_pos1 + 1
-            chain_lengths[chain_id] = chain_pos + 1
-            t.data.chain_id = chain_id
-            t.chain_pos.resize(chain_lengths.size(), -1)
-            t.chain_pos[chain_id] = chain_pos
-
-        cdef int nchains = chain_lengths.size()
-        assume_not_none(event_times)
-        for _t in event_times:
-            t = <EventTime>_t
-            t.chain_pos.resize(nchains, -1)
-            prev = t.prev
-            if prev is not None:
-                update_chain_pos(t, prev, nchains)
-            wait_for = t.wait_for
-            if wait_for is not None:
-                update_chain_pos(t, wait_for, nchains)
-
-        return 0
+        timemanager_finalize(self, None)
 
     cdef long long compute_all_times(self, unsigned age, py_object &pyage) except -1:
-        status = self.status.get()
-        if not status.finalized:
-            PyErr_Format(PyExc_RuntimeError, "Event times not finalized")
-        cdef long long max_time = 0
-        event_times = self.event_times
-        cdef int ntimes = PyList_GET_SIZE(event_times)
-        self.time_values.resize(ntimes)
-        cppfill(self.time_values.begin(), self.time_values.end(), -1)
-        assume_not_none(event_times)
-        for t in event_times:
-            tv = get_time_value(<EventTime>t, -1, age, pyage, self.time_values)
-            if tv > max_time:
-                max_time = tv
-        return max_time
-
-cdef void update_chain_pos(EventTime self, EventTime prev, int nchains) noexcept:
-    cdef int cid = self.data.chain_id
-    for i in range(nchains):
-        if i == cid:
-            continue
-        prev_pos = prev.chain_pos[i]
-        if self.chain_pos[i] < prev_pos:
-            self.chain_pos[i] = prev_pos
-
-# If the base time has a static value, the returned time values will be the actual
-# time point of the EventTime. If the base time does not have a static value
-# the time offset relative to the base time is returned.
-# This is so that if the base time is not statically known,
-# we can compute the diff without computing the base time,
-# while if the base time is known, we can use the static values in the computation
-cdef long long get_time_value(EventTime self, int base_id, unsigned age,
-                              py_object &pyage, vector[long long] &cache) except -1:
-    cdef int tid = self.data.id
-    if tid == base_id:
-        if self.data.is_static():
-            return self.data._get_static()
-        return 0
-    assert tid > base_id
-    cdef long long value = cache[tid]
-    if value >= 0:
-        return value
-    # The time manager should've been finalized and
-    # no time should be floating anymore
-    assert not self.data.floating
-
-    if self.data.is_static():
-        # If we have a static value it means that the base time has a static value
-        # In this case, we are returning the full time and there's no need to
-        # compute the offset from the base time.
-        static_value = self.data._get_static()
-        cache[tid] = static_value
-        return static_value
-
-    cdef EventTime prev = self.prev
-    cdef long long prev_val = 0
-    if prev is not None:
-        prev_val = get_time_value(prev, base_id, age, pyage, cache)
-
-    cdef bint cond = get_cond_val(<PyObject*>self.cond, age, pyage)
-    cdef long long offset = 0
-    if cond:
-        p_rt_offset = self.data.get_rt_offset()
-        if p_rt_offset != NULL:
-            rt_eval_throw(<RuntimeValue>p_rt_offset, age, pyage,
-                          event_time_key(<void*>self))
-            offset = (<RuntimeValue>p_rt_offset).cache_val.i64_val
-            if offset < 0:
-                bb_err_format(ValueError, event_time_key(<void*>self),
-                              "Time delay cannot be negative")
-        else:
-            offset = <long long>self.data.get_c_offset()
-
-    cdef EventTime wait_for = self.wait_for
-    if wait_for is None:
-        # When wait_for is None, the offset is added to the previous time
-        value = prev_val + offset
-        cache[tid] = value
-        return value
-
-    # Otherwise, the wait_for is added to the wait_for time.
-    value = prev_val
-    cdef long long wait_for_val
-    if cond:
-        # Do not try to evaluate wait_for unless the condition is true
-        # When a base_id is supplied, the wait_for event time may not share
-        # this base if the condition isn't true.
-        wait_for_val = get_time_value(wait_for, base_id, age, pyage, cache) + offset
-        value = max(value, wait_for_val)
-    cache[tid] = value
-    return value
+        return timemanager_compute_all_times(self, age, pyage, None)
 
 @cython.auto_pickle(False)
 @cython.c_api_binop_methods(True)
@@ -318,41 +95,6 @@ cdef class EventTime:
         return str(self)
 
 
-cdef EventTime find_common_root(EventTimeDiff self, unsigned age, py_object &pyage):
-    cdef cppmap[int,void*] frontier
-    if not self.t1.manager_status.get().finalized:
-        PyErr_Format(PyExc_RuntimeError, "Event times not finalized")
-    frontier[self.t1.data.id] = <void*>self.t1
-    frontier[self.t2.data.id] = <void*>self.t2
-    while frontier.size() > 1:
-        it = predec(frontier.end())
-        t = <EventTime>deref(it).second
-        frontier.erase(it)
-        prev = t.prev
-        if prev is None:
-            return # Found the start of the experiment
-        frontier[prev.data.id] = <void*>prev
-        wait_for = t.wait_for
-        if wait_for is not None:
-            if not get_cond_val(<PyObject*>t.cond, age, pyage):
-                continue
-            frontier[wait_for.data.id] = <void*>wait_for
-    return <EventTime>deref(frontier.begin()).second
-
-cdef double timediff_eval(EventTimeDiff self, unsigned age) except? -100.0:
-    cdef py_object pyage
-    cdef EventTime common_root = find_common_root(self, age, pyage)
-    cdef int base_id = -1
-    if common_root is not None:
-        base_id = common_root.data.id
-    t1 = self.t1
-    t2 = self.t2
-    cdef vector[long long] cache
-    cache.resize(t1.manager_status.get().ntimes, -1)
-    cdef double diff = (get_time_value(t1, base_id, age, pyage, cache) -
-                        get_time_value(t2, base_id, age, pyage, cache))
-    return diff / c_time_scale
-
 @cython.internal
 @cython.final
 cdef class EventTimeDiff(ExternCallback):
@@ -361,13 +103,7 @@ cdef class EventTimeDiff(ExternCallback):
     cdef bint in_eval
 
     def __call__(self, unsigned age, /):
-        if self.in_eval:
-            PyErr_Format(PyExc_ValueError, "Recursive value dependency detected.")
-        self.in_eval = True
-        try:
-            return timediff_eval(self, age)
-        finally:
-            self.in_eval = False
+        return timediff_eval(self, age)
 
     def __str__(self):
         return f'(T[{self.t1.data.id}] - T[{self.t2.data.id}])'
--- brassboard_seq/src/event_time.cpp
+++ brassboard_seq/src/event_time.cpp
@@ -23,6 +23,7 @@
 #include "assert.h"
 
 #include <array>
+#include <unordered_set>
 
 namespace brassboard_seq::event_time {
 
@@ -93,6 +94,272 @@ static PyObject *str_time(long long t)
     *end = ' ';
     memcpy(&end[1], unit, strlen(unit) + 1);
     return PyUnicode_FromString(str.data());
+}
+
+static inline void visit_time(auto self, auto t, auto &visited)
+{
+    auto id = t->data.id;
+    if (id < 0)
+        return;
+    if (t->data.floating)
+        bb_throw_format(PyExc_RuntimeError, event_time_key(t),
+                        "Event time still floating");
+    if (visited.contains(id))
+        bb_throw_format(PyExc_ValueError, event_time_key(t), "Time loop detected");
+    visited.insert(id);
+    long long static_offset;
+    auto rt_offset = t->data.get_rt_offset();
+    auto cond = t->cond;
+    if (cond == Py_True) {
+        static_offset = rt_offset ? -1 : t->data.get_c_offset();
+    }
+    else if (cond == Py_False || (!rt_offset && t->data.get_c_offset() == 0)) {
+        static_offset = 0;
+    }
+    else {
+        static_offset = -1;
+    }
+    long long static_prev = 0;
+    long long static_wait_for = 0;
+    if (auto prev = t->prev; (PyObject*)prev != Py_None) {
+        visit_time(self, prev, visited);
+        static_prev = prev->data.get_static();
+    }
+    if (auto wait_for = t->wait_for; (PyObject*)wait_for != Py_None) {
+        visit_time(self, wait_for, visited);
+        static_wait_for = wait_for->data.get_static();
+        if (cond == Py_False) {
+            if (static_prev != -1) {
+                t->data.set_static(static_prev);
+            }
+        }
+        else if (cond == Py_True && static_wait_for != -1 &&
+                 static_offset != -1 && static_prev != -1) {
+            t->data.set_static(std::max(static_prev, static_wait_for + static_offset));
+        }
+    }
+    else if (static_offset != -1 && static_prev != -1) {
+        t->data.set_static(static_prev + static_offset);
+    }
+    t->data.id = -1;
+    pylist_append(self->event_times, (PyObject*)t);
+}
+
+static inline void update_chain_pos(auto self, auto prev, int nchains)
+{
+    auto cid = self->data.chain_id;
+    for (int i = 0; i < nchains; i++) {
+        if (i == cid)
+            continue;
+        self->chain_pos[i] = std::max(self->chain_pos[i], prev->chain_pos[i]);
+    }
+}
+
+// If the base time has a static value, the returned time values will be the actual
+// time point of the EventTime. If the base time does not have a static value
+// the time offset relative to the base time is returned.
+// This is so that if the base time is not statically known,
+// we can compute the diff without computing the base time,
+// while if the base time is known, we can use the static values in the computation
+static inline long long get_time_value(auto self, int base_id, unsigned age,
+                                       py_object &pyage, std::vector<long long> &cache)
+{
+    auto tid = self->data.id;
+    if (tid == base_id)
+        return self->data.is_static() ? self->data._get_static() : 0;
+    assert(tid > base_id);
+    auto value = cache[tid];
+    if (value >= 0)
+        return value;
+    // The time manager should've been finalized and
+    // no time should be floating anymore
+    assert(!self->data.floating);
+
+    if (self->data.is_static()) {
+        // If we have a static value it means that the base time has a static value
+        // In this case, we are returning the full time and there's no need to
+        // compute the offset from the base time.
+        auto static_value = self->data._get_static();
+        cache[tid] = static_value;
+        return static_value;
+    }
+
+    long long prev_val = 0;
+    if (auto prev = self->prev; (PyObject*)prev != Py_None)
+        prev_val = get_time_value(prev, base_id, age, pyage, cache);
+
+    auto cond = get_cond_val(self->cond, age, pyage);
+    long long offset = 0;
+    if (cond) {
+        auto rt_offset = (rtval::_RuntimeValue*)self->data.get_rt_offset();
+        if (rt_offset) {
+            rt_eval_throw(rt_offset, age, pyage, event_time_key(self));
+            offset = rt_offset->cache_val.i64_val;
+            if (offset < 0) {
+                bb_throw_format(PyExc_ValueError, event_time_key(self),
+                                "Time delay cannot be negative");
+            }
+        }
+        else {
+            offset = self->data.get_c_offset();
+        }
+    }
+
+    if (auto wait_for = self->wait_for; (PyObject*)wait_for == Py_None) {
+        // When wait_for is None, the offset is added to the previous time
+        value = prev_val + offset;
+    }
+    else {
+        // Otherwise, the wait_for is added to the wait_for time.
+        value = prev_val;
+        // Do not try to evaluate wait_for unless the condition is true
+        // When a base_id is supplied, the wait_for event time may not share
+        // this base if the condition isn't true.
+        if (cond) {
+            value = std::max(value, get_time_value(wait_for, base_id, age,
+                                                   pyage, cache) + offset);
+        }
+    }
+
+    cache[tid] = value;
+    return value;
+}
+
+template<typename EventTime>
+static inline void timemanager_finalize(auto self, EventTime*)
+{
+    if (self->status->finalized)
+        py_throw_format(PyExc_RuntimeError, "Event times already finalized");
+    self->status->finalized = true;
+    auto event_times = pylist_new(0);
+    py_object old_event_times(self->event_times); // steal reference
+    self->event_times = event_times;
+    std::unordered_set<int> visited;
+    // First, topologically order the times
+    for (auto [i, t]: pylist_iter(old_event_times.get()))
+        visit_time(self, (EventTime*)t, visited);
+
+    std::vector<int> chain_lengths;
+    for (auto [tid, t]: pylist_iter<EventTime>(event_times)) {
+        t->data.id = tid;
+        int chain_id1 = -1;
+        int chain_pos1;
+        if (auto prev = t->prev; (PyObject*)prev != Py_None) {
+            chain_id1 = prev->data.chain_id;
+            chain_pos1 = prev->chain_pos[chain_id1];
+            if (chain_pos1 + 1 != chain_lengths[chain_id1]) {
+                // Not the tail of the chain
+                chain_id1 = -1;
+            }
+        }
+        int chain_id2 = -1;
+        int chain_pos2;
+        if (auto wait_for = t->wait_for; (PyObject*)wait_for != Py_None) {
+            chain_id2 = wait_for->data.chain_id;
+            chain_pos2 = wait_for->chain_pos[chain_id2];
+            if (chain_pos2 + 1 != chain_lengths[chain_id2]) {
+                // Not the tail of the chain
+                chain_id2 = -1;
+            }
+        }
+        int chain_id;
+        int chain_pos;
+        if (chain_id1 == -1) {
+            if (chain_id2 == -1) {
+                // New chain
+                chain_id = chain_lengths.size();
+                chain_pos = 0;
+                chain_lengths.push_back(0);
+            }
+            else {
+                chain_id = chain_id2;
+                chain_pos = chain_pos2 + 1;
+            }
+        }
+        else if (chain_id2 == -1) {
+            chain_id = chain_id1;
+            chain_pos = chain_pos1 + 1;
+        }
+        else if (chain_pos2 > chain_pos1) {
+            chain_id = chain_id2;
+            chain_pos = chain_pos2 + 1;
+        }
+        else {
+            chain_id = chain_id1;
+            chain_pos = chain_pos1 + 1;
+        }
+        chain_lengths[chain_id] = chain_pos + 1;
+        t->data.chain_id = chain_id;
+        t->chain_pos.resize(chain_lengths.size(), -1);
+        t->chain_pos[chain_id] = chain_pos;
+    }
+
+    int nchains = chain_lengths.size();
+    for (auto [tid, t]: pylist_iter<EventTime>(event_times)) {
+        t->chain_pos.resize(nchains, -1);
+        if (auto prev = t->prev; (PyObject*)prev != Py_None)
+            update_chain_pos(t, prev, nchains);
+        if (auto wait_for = t->wait_for; (PyObject*)wait_for != Py_None)
+            update_chain_pos(t, wait_for, nchains);
+    }
+}
+
+template<typename EventTime>
+static inline long long timemanager_compute_all_times(auto self, unsigned age,
+                                                      py_object &pyage, EventTime*)
+{
+    if (!self->status->finalized)
+        py_throw_format(PyExc_RuntimeError, "Event times not finalized");
+    long long max_time = 0;
+    auto event_times = self->event_times;
+    int ntimes = PyList_GET_SIZE(event_times);
+    self->time_values.resize(ntimes);
+    std::ranges::fill(self->time_values, -1);
+    for (auto [i, t]: pylist_iter<EventTime>(event_times))
+        max_time = std::max(max_time, get_time_value(t, -1, age, pyage,
+                                                     self->time_values));
+    return max_time;
+}
+
+// Returns borrowed reference
+template<typename EventTime>
+static inline int eventtime_find_base_id(EventTime *t1, EventTime *t2,
+                                         unsigned age, py_object &pyage)
+{
+    std::map<int,EventTime*> frontier;
+    if (!t1->manager_status->finalized)
+        py_throw_format(PyExc_RuntimeError, "Event times not finalized");
+    frontier[t1->data.id] = t1;
+    frontier[t2->data.id] = t2;
+    while (frontier.size() > 1) {
+        auto it = --frontier.end();
+        auto t = it->second;
+        frontier.erase(it);
+        auto prev = t->prev;
+        if ((PyObject*)prev == Py_None)
+            return -1; // Found the start of the experiment
+        frontier[prev->data.id] = prev;
+        if (auto wait_for = t->wait_for;
+            (PyObject*)wait_for != Py_None && get_cond_val(t->cond, age, pyage)) {
+            frontier[wait_for->data.id] = wait_for;
+        }
+    }
+    return frontier.begin()->second->data.id;
+}
+
+static inline double timediff_eval(auto self, unsigned age)
+{
+    auto t1 = self->t1;
+    auto t2 = self->t2;
+    if (self->in_eval)
+        py_throw_format(PyExc_ValueError, "Recursive value dependency detected.");
+    self->in_eval = true;
+    ScopeExit reset_eval([&] { self->in_eval = false; });
+    py_object pyage;
+    int base_id = eventtime_find_base_id(t1, t2, age, pyage);
+    std::vector<long long> cache(t1->manager_status->ntimes, -1);
+    return double(get_time_value(t1, base_id, age, pyage, cache) -
+                  get_time_value(t2, base_id, age, pyage, cache)) / time_scale;
 }
 
 static traverseproc event_time_base_traverse;
