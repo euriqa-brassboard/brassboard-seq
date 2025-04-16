2a0be689a49b6f27ca94328ab8a5b8c2fabcb0cc
Yichao Yu <yyc1992@gmail.com>
2025-04-15 20:20:24 -0400

    Clean up _utils module initialization

--- brassboard_seq/_utils.cpp
+++ brassboard_seq/_utils.cpp
@@ -39,15 +39,15 @@ static PyModuleDef _utils_module = {
 };
 
 #if PY_VERSION_HEX >= 0x030a0000
-static void pymodule_addobjectref(py::mod m, const char *name, PyObject *value)
+static void pymodule_addobjectref(py::mod m, const char *name, py::ptr<> value)
 {
     throw_if(PyModule_AddObjectRef((PyObject*)m, name, value) < 0);
 }
 #else
-static void pymodule_addobjectref(py::mod m, const char *name, PyObject *value)
+static void pymodule_addobjectref(py::mod m, const char *name, py::ptr<> value)
 {
     py::ref v(py::newref(value));
-    throw_if(PyModule_AddObject((PyObject*)m, name, value.get()) < 0);
+    throw_if(PyModule_AddObject((PyObject*)m, name, v.get()) < 0);
     v.rel();
 }
 #endif
@@ -56,29 +56,40 @@ PyMODINIT_FUNC
 PyInit__utils(void)
 {
     return cxx_catch([&] {
-        auto yaml_sprint = py::new_cfunc(&yaml::sprint_method, nullptr,
-                                         "brassboard_seq.yaml"_py);
-        auto parampack_get_visited = py::new_cfunc(&scan::parampack_get_visited_method,
-                                                   nullptr, "brassboard_seq.scan"_py);
-        auto parampack_get_param = py::new_cfunc(&scan::parampack_get_param_method,
-                                                 nullptr, "brassboard_seq.scan"_py);
         auto m = py::new_module(&_utils_module);
-        pymodule_addobjectref(m, "RuntimeValue",
-                              (PyObject*)&rtval::RuntimeValue::Type);
-        pymodule_addobjectref(m, "ExternCallback",
-                              (PyObject*)&rtval::ExternCallback::Type);
+        // config
+        pymodule_addobjectref(m, "Config", (PyObject*)&config::Config::Type);
+
+        // event_time
         pymodule_addobjectref(m, "TimeManager",
                               (PyObject*)&event_time::TimeManager::Type);
         pymodule_addobjectref(m, "EventTime",
                               (PyObject*)&event_time::EventTime::Type);
-        pymodule_addobjectref(m, "Config", (PyObject*)&config::Config::Type);
+
+        // rtprop
         pymodule_addobjectref(m, "CompositeRTProp",
                               (PyObject*)&rtprop::CompositeRTProp_Type);
         pymodule_addobjectref(m, "RTProp", (PyObject*)&rtprop::RTProp_Type);
+
+        // rtval
+        pymodule_addobjectref(m, "RuntimeValue",
+                              (PyObject*)&rtval::RuntimeValue::Type);
+        pymodule_addobjectref(m, "ExternCallback",
+                              (PyObject*)&rtval::ExternCallback::Type);
+
+        // scan
         pymodule_addobjectref(m, "ParamPack", (PyObject*)&scan::ParamPack::Type);
-        pymodule_addobjectref(m, "parampack_get_visited", parampack_get_visited.get());
-        pymodule_addobjectref(m, "parampack_get_param", parampack_get_param.get());
-        pymodule_addobjectref(m, "yaml_sprint", yaml_sprint.get());
+        pymodule_addobjectref(m, "parampack_get_visited",
+                              py::new_cfunc(&scan::parampack_get_visited_method,
+                                            nullptr, "brassboard_seq.scan"_py));
+        pymodule_addobjectref(m, "parampack_get_param",
+                              py::new_cfunc(&scan::parampack_get_param_method,
+                                            nullptr, "brassboard_seq.scan"_py));
+
+        // yaml
+        pymodule_addobjectref(m, "yaml_sprint",
+                              py::new_cfunc(&yaml::sprint_method, nullptr,
+                                            "brassboard_seq.yaml"_py));
         return m;
     });
 }
